facb41a8223e1f6152b742e867b6d9cd2ba73876
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-01-06 17:23:47 -0600

    spacing

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -9,17 +9,17 @@ function BackgroundCircles({}: Props) {
       opacity: 0,
      }}
      animate={{
-      scale: [1,2,2,3,1],
-      opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+      scale: [1,2,3,1],
+      opacity: [0.1, 0.2, 0.4,  .5],
       borderRadius: ["20%", "20%", "50%", "80%", "20%"]
      }}
      transition={{
-      duration: 2.5,
+      duration: 5.5,
      }}
      className='relative flex justify-center items-center'
      >
-        <div className='absolute border border-gray-300 rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='absolute border border-gray-300 rounded-full h-[300px] w-[300px] mt-52 '/>
+        <div className='absolute border border-gray-500 rounded-full h-[200px] w-[200px] mt-52 animate-pulse'/>
+        <div className='absolute border border-gray-500 rounded-full h-[300px] w-[300px] mt-52 animate-pulse'/>
         <div className='absolute border border-gray-500 rounded-full h-[500px] w-[500px] mt-52 '/>
         {/* <div className='rounded-full border border-green opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse' /> */}
         <div className='rounded-full border border-green  h-[800px] w-[800px] absolute mt-52 animate-pulse' />
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,7 +15,7 @@ function Contact({}: Props) {
     const { register, handleSubmit } = useForm<Inputs>();
 
     const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
-    
+
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
     <h3 className='absolute top-24 uppercase tracking-[15px] text-gray-500 text-2xl'>
@@ -42,14 +42,16 @@ function Contact({}: Props) {
                 </div>
             </div>
 
-            <form className='flex flex-col space-y-2 w-fit mx-auto'>
+            <form 
+             onSubmit={handleSubmit(onSubmit)}
+             className='flex flex-col space-y-2 w-fit mx-auto'>
                 <div className='flex space-x-2'>
-                    <input placeholder="Name" className='contactInput' type="text" />
-                    <input placeholder="Email" className='contactInput' type="email" />
+                    <input {...register('name')} placeholder="Name" className='contactInput' type="text" />
+                    <input {...register('email')} placeholder="Email" className='contactInput' type="email" />
                 </div>
 
-                <input placeholder="Subject" className='contactInput' type='text' />
-                <textarea placeholder="Message" className='contactInput'/>
+                <input {...register('subject')} placeholder="Subject" className='contactInput' type='text' />
+                <textarea {...register('message')} placeholder="Message" className='contactInput'/>
                 <button type="submit" className='bg-green-500 py-5 px-10 rounded-md text-black font-bold text-lg'>Submit</button>
             </form>
         </div>
