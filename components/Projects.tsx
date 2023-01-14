ac0f8336a7c581d57233e80f86581de5aee7f91f
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-01-13 18:17:42 -0600

    content

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,6 +12,7 @@ function Projects({}: Props) {
        title: "E-Commerce Art Site",
         stack: "NEXT.js | MongoDB | Axios | ",
         img: "https://i.imgur.com/SQJgpm7.png",
+        img2: "https://i.imgur.com/HQMebwS.png",
         description: "Full stack e-commerce site with admin dashboard and PayPal integration. Fully responsive with framer motion animation.",
         url: "https://faithnguyenart.vercel.app/",
         git:""
@@ -21,7 +22,9 @@ function Projects({}: Props) {
         
          title: "Modern Blog",
         stack: "NEXT.js 13 | Sanity | TypeScript | Tailwind CSS",
-        img: "https://i.imgur.com/GC3SlNH.png",
+        img: "https://i.imgur.com/2CKj8q3.png",
+        img2: "https://i.imgur.com/eQ6RWhw.png",
+
         description: "Fully responsive blog. Static and Dynamic data handling, preview mode and studio with Sanity v.3, ",
         url: "https://faithnguyenblog.vercel.app/",
         git:""
@@ -30,7 +33,7 @@ function Projects({}: Props) {
         id: 3,
         title: "Meta Messenger App clone",
         stack: "NEXT.js 13 | Sanity | TypeScript | Tailwind CSS",
-        img: "https://cdn.dribbble.com/userupload/4221657/file/original-47c6fe97504f4a60f4392c7ce183453f.png?compress=1&resize=1024x576",
+        img: "https://i.imgur.com/o8ZG7RG.png",
         description: "lorem",
         url: "",
         git:""
@@ -61,10 +64,12 @@ function Projects({}: Props) {
             Projects
         </h3>
 
-        <div className='relative w-full h-11/12 flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[rgb(107,43,43)]/80 mb-[15px] px-5'>
+        <div className='relative w-full h-11/12 flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[rgb(107,43,43)]/80 mb-[15px] px-5 '>
             {/* projects */}
             {projects.map((project, i)=> (
-                <div key={i} className='lg:flex-row w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center px-24 bg-gradient-to-b from-[rgb(107,43,43)]/50 '>
+                <div key={i} className='lg:flex-row w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center px-24 bg-gradient-to-b from-[rgb(107,43,43)]/40 '>
+                    <Link href={project.url} className="z-20">
+
                     <motion.img
                         initial={{
                             y:0,
@@ -78,9 +83,11 @@ function Projects({}: Props) {
                     //    transition={{ duration: 1.2 }}
                     //    whileInView={{ opacity: 1, y: 0}}
                     //    viewport={{ once: true }}
-                       src={project.img}
+                    src={project.img}
                        className=' sm:max-w-[400px] lg:max-w-[500px] max-h-[400px] lg:pb-14'
                        />
+                    </Link>
+                
                     <div className='space-y-5 lg:py-7 md:mx-14 xl:mx-28'>
                         <h4 className='text-xl xl:text-xl font-light text-center flex flex-col'>
                             <span className='text-4xl uppercase font-semibold pb-2 tracking-widest'>{project.title}</span> {project.stack}
